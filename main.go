b1c0fdddf97e043590db9f4be9cceaa87faf2838
Tomoo Amano <amano@diverse-inc.com>
2015-06-15 18:32:12 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "github.com/elazarl/goproxy"
+    "log"
+    "net/http"
+)
+
+func main() {
+    proxy := goproxy.NewProxyHttpServer()
+    proxy.Verbose = true
+    log.Fatal(http.ListenAndServe(":9090", proxy))
+}
+
