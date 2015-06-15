2f7f0387073477f6f0221e41fbadefcef47ec5dc
Tomoo Amano <amano@diverse-inc.com>
2015-06-15 19:05:59 +0900

    basic auth

---
+++ README.md
@@ -0,0 +1,22 @@
+# atto
+
+super mini proxy server.
+
+## install
+
+    go get github.com/sheercat/yaproxy
+
+## Getting Started
+
+   $ yaproxy
+
+   $ yaproxy --port=9090
+
+   $ yaproxy --user=basic --pass=auth
+
+## License
+
+yaproxy licensed under the MIT
+
+
+
--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-    "github.com/elazarl/goproxy"
-    "log"
-    "net/http"
+	"flag"
+	"github.com/elazarl/goproxy"
+	"github.com/elazarl/goproxy/ext/auth"
+	"log"
+	"net/http"
 )
 
-func main() {
-    proxy := goproxy.NewProxyHttpServer()
-    proxy.Verbose = true
-    log.Fatal(http.ListenAndServe(":9090", proxy))
-}
+var portNumber = flag.String("port", "9090", "port number.")
+var basicAuthUser = flag.String("user", "", "basic auth user name")
+var basicAuthPass = flag.String("pass", "", "basic auth user pass")
 
+func main() {
+	flag.Parse()
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = true
+	if *basicAuthUser != "" && *basicAuthPass != "" {
+		log.Println("auth basic")
+		auth.ProxyBasic(proxy, "yaproxy", func(user, pass string) bool {
+			log.Println("auth basic do")
+			return user == *basicAuthUser && pass == *basicAuthPass
+		})
+	}
+
+	log.Println("listen:" + *portNumber)
+	log.Fatal(http.ListenAndServe(":"+*portNumber, proxy))
+}
